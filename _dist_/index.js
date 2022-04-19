9e6135d9aa2c2b455bf636a8ef87d7cee5fd88c8
cesanagu <cesanagu@users.noreply.github.com>
2022-04-19 15:13:27 +0000

    deploy: 3f29de124caa84a4cbdae48456c3427d93489c19

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,77 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+import { registerImage } from "./lazy.js";
+
+// const baseUrl = "https://randomfox.ca/images/"
+
+// const appNode = document.querySelector("#app");
+// appNode.addEventListener("click", (event) => {
+//     if (event.target.button){
+//         items.append(...items)
+//     }
+// })
+
+// const random1 = () => {
+//     Math.random() * 120;
+// }
+
+// window.fetch(`${baseUrl}`).then(response => response.json())
+// .then(responseJSON =>{
+//     const items =[];
+//     responseJSON.data.forEach(item => {
+
+//         const imagen = document.createElement('img');
+//         imagen.src = `${baseUrl}${random}+.jpg`
+
+//         const container = document.createElement('div')
+//         container.append(imagen);
+
+//     items.push(container)
+//     })
+//     items.append(...items)
+// })
+
+// ----------------------------------------------------------------
+
+const minimum = 1;
+const maximum = 122;
+const random = () => Math.floor(Math.random() * (maximum - minimum))+ minimum;
+
+const createImage = () => {
+
+    const container = document.createElement('div')
+    container.className = 'p-4'
+
+    const imagen = document.createElement('img');
+    imagen.className = 'mx-auto my-5 rounded bg-gray-200';
+    imagen.width = "320";
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const mountNode = document.getElementById('images');
+
+
+// newImageButton
+const addButton = document.querySelector('button');
+
+const addImage = () => {
+    const newImage = createImage();
+    mountNode.appendChild(newImage);
+    // mountNode.insertAdjacentElement('afterbegin', newImage);
+    registerImage(newImage);
+};
+
+addButton.addEventListener("click", addImage);
+
+// removeImageButton
+const removeButton = document.getElementById('btn-clean');
+
+const clearImage = () => {
+    mountNode.replaceChildren();
+};
+
+removeButton.addEventListener("click", clearImage);
+
